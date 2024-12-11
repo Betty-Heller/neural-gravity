2a192036ad5ab1208969a6b37cfcd5c5605efedd
Betty-Heller <57674771+Betty-Heller@users.noreply.github.com>
2024-12-11 11:35:22 +0100

    Added definition files for creating singularity containers

---
+++ Container/Container_GPU_TF_02/build_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# build custom singularity container:
+singularity build Container_GPU_TF_02.sif Container_GPU_TF_02.def
---
+++ Container/Container_no_GPU_01_03/build_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# build custom singularity container:
+singularity build Container_no_GPU_01_03.sif Container_no_GPU_01_03.def
